d25ebe23dffb6d4747c249c7f5f41b1e9278ac56
Hritik Munde <hritik16munde@gmail.com>
2026-09-07 15:58:00 -0700

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return "__EMPTY_STR__"
+        encoded_string = ""
+        for s in strs:
+            encoded_string += f"{len(s)}-{s}"
+        return encoded_string
+
+    def decode(self, s: str) -> List[str]:
+        if s == "__EMPTY_STR__":
+            return []
+        i = 0
+        decoded_strs = []
+        while i < len(s):
+            j = s.find("-", i)
+            length = int(s[i:j])
+            str_start = j + 1
+            str_end = str_start + length
+            decoded_strs.append(s[str_start:str_end])
+            i = str_end
+        return decoded_strs
