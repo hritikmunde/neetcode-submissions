d41852df49497e7e9a5fbf44145e6aad57c30a29
Hritik Munde <hritik16munde@gmail.com>
2026-09-17 18:36:15 -0700

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        bracket_pairs = {"{": "}", "(": ")", "[": "]"}
+        stack = []
+        for c in s:
+            if c in bracket_pairs.keys():
+                stack.append(c)
+                
+            else:
+                if stack and c == bracket_pairs[stack[-1]]:
+                    stack.pop()
+                else:
+                    return False
+        return True if not stack else False
